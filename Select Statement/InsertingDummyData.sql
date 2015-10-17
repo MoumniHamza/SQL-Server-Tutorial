dc93ea23637fd8e457f02ce37c2ea568990ddae9
Hamza Moumni <94747@supinfo.com>
2015-10-17 14:30:36 -0700

    Working with the select statement

---
+++ Select Statement/InsertingDummyData.sql
@@ -0,0 +1,15 @@
+-- To work with the select lets insert some dummy data into our table
+
+
+Use sample2
+GO
+
+INSERT INTO tblGender
+values (1, 'boy'),(2, 'girl'),(3, 'unknown')
+
+INSERT INTO tblEmployee
+VALUES (1,'Hamza','hamza@com',2, 25), (2,'Sara','sara@com',1, 23)
+GO
+
+
+
